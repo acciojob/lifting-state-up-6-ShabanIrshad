963510964e09975773a1e2d4ca9ae377c6553cee
Shaban Irshad <shabanirshad408@gmail.com>
2026-01-18 10:46:58 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
-
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
 
 const App = () => {
+  const [todos,setTodos]=useState([
+    {
+      text:'Learn React',
+      complete:false
+    },
+    {
+      text:'Build React App',
+      complete:false
+    },
+    {
+      text:'Deploye React App',
+      complete:false
+    }
+  ]);
+
+ 
+  const handleComplete = (index) => {
+  const updatedTodos = todos.map((todo, i) =>
+    i === index ? { ...todo, complete: !todo.complete } : todo
+  );
+
+  setTodos(updatedTodos);
+};
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>Parent Component</h1>
+       <TodoList todos={todos} handleComplete={handleComplete}/>
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TodoList =(props)=>{
+    const {todos,handleComplete}=props;
+    console.log(todos);
+    return (
+        
+        <>
+            <h2>Child Component</h2>
+            <ul>
+                 {todos.map((todo,key)=>{
+                    return <li><div key={key}><p>{todo.text}</p>{todo.complete?'':<button onClick={()=>handleComplete(key)}>Complete</button>}</div></li>
+                 })}
+            </ul>
+           
+        </>
+
+    );
+
+}
+
+export default TodoList;
